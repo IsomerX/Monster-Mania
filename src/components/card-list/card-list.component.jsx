4c4fa9bdb9a19b1aac17c8a87107369395ae5386
isomerx <dhruvbakshi12343@gmail.com>
2021-06-24 21:12:10 +0530

    initializing

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./card-list.styles.css";
+import { Card } from "../card/card.component";
+
+export const CardList = (prop) => {
+  return (
+    <div className="card-list">
+      {prop.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
